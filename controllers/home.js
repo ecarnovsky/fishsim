fdb70534c32e7aa36433276fae20684a83a0d186
ecarnovsky <ecarnovsky@gmail.com>
2023-12-24 02:31:11 -0500

    Added the username to the ejs files

--- controllers/home.js
+++ controllers/home.js
@@ -1,6 +1,6 @@
 module.exports = {
     getIndex: (req,res)=>{
-        res.render('index.ejs')
+        res.render('index.ejs', {user: req.user})
     },
     getFish: (req,res)=> {
         res.render('fish.ejs')
