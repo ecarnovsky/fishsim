c836d3261b09d51fe23268aea1c9578d22e3dd7e
ecarnovsky <ecarnovsky@gmail.com>
2024-01-03 23:11:55 -0500

    Added some tags and text to forums.ejs

--- fish/fish-class.js
+++ fish/fish-class.js
@@ -80,6 +80,113 @@ class FishClass{
         {'abbreviation':'h', 'probability': 0.60}
     ]
 
+    static GuppyColorAlleles = [
+        [
+            // black pigment
+            {'abbreviation':'M', 'probability': 0.70},
+            // no black pigment
+            {'abbreviation':'m', 'probability': 0.30}
+        ],
+        [
+            // allows blue pigment
+            {'abbreviation':'G', 'probability': 0.30},
+            // no blue pigment
+            {'abbreviation':'g', 'probability': 0.70}
+        ],
+        [
+            // red pigment
+            {'abbreviation':'E', 'probability': 0.70},
+            // no red pigment
+            {'abbreviation':'e', 'probability': 0.30}
+        ],
+        [
+            // yellow pigment
+            {'abbreviation':'X', 'probability': 0.70},
+            // no yellow pigment
+            {'abbreviation':'x', 'probability': 0.30}
+        ],
+        [
+            //  no moscow
+            {'abbreviation':'O', 'probability': 0.90},
+            // moscow
+            {'abbreviation':'o', 'probability': 0.10}
+        ],
+        [
+            // no koi
+            {'abbreviation':'K', 'probability': 0.98},
+            // koi
+            {'abbreviation':'k', 'probability': 0.02}
+        ],
+        [
+            // tuxedo
+            {'abbreviation':'T', 'probability': 0.30},
+            // none
+            {'abbreviation':'t', 'probability': 0.70}
+        ],
+        [
+            // none
+            {'abbreviation':'N', 'probability': 0.80},
+            // half black (including tail)
+            {'abbreviation':'nh', 'probability': 0.19},
+            // full black
+            {'abbreviation':'n', 'probability': 0.01}
+        ],
+        [
+            // tail is black
+            {'abbreviation':'A', 'probability': 0.10},
+            // none
+            {'abbreviation':'a', 'probability': 0.90}
+        ],
+        [
+            // mosaic
+            {'abbreviation':'P', 'probability': 0.40},
+            // leopard
+            {'abbreviation':'pl', 'probability': 0.40},
+            // lace
+            {'abbreviation':'pa', 'probability': 0.09},
+            // grass
+            {'abbreviation':'pg', 'probability': 0.04},
+            // none
+            {'abbreviation':'pn', 'probability': 0.07}
+        ],
+        [
+            // snakeskin
+            {'abbreviation':'S', 'probability': 0.15},
+            // none
+            {'abbreviation':'s', 'probability': 0.85}
+        ],
+        [
+            // normal
+            {'abbreviation':'R', 'probability': 0.99},
+            // full red
+            {'abbreviation':'r', 'probability': 0.01}
+        ],
+        [
+            // normal
+            {'abbreviation':'Y', 'probability': 0.95},
+            // albino 1
+            {'abbreviation':'y', 'probability': 0.05}
+        ],
+        [
+            // normal
+            {'abbreviation':'Z', 'probability': 0.95},
+            // albino 2
+            {'abbreviation':'z', 'probability': 0.05}
+        ],
+        [
+            // normal
+            {'abbreviation':'B', 'probability': 0.99},
+            // blue, no red, no yellow
+            {'abbreviation':'b', 'probability': 0.01}
+        ],
+        [
+            // normal
+            {'abbreviation':'U', 'probability': 0.99},
+            // blue, reduced red, no yellow
+            {'abbreviation':'u', 'probability': 0.01}
+        ],
+    ]
+
     static generateFinDescriptionStr(finGenome, tailSizeGenome, swordTypeGenome){
 
         let allelesArray = finGenome.split(/\s|-/)
