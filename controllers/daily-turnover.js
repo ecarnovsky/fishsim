a6873569f66dcb78204e306aa98c716466eec56c
ecarnovsky <ecarnovsky@gmail.com>
2024-02-22 22:13:28 -0500

    Made it so a key is needed to run the cron jobs

--- controllers/daily-turnover.js
+++ controllers/daily-turnover.js
@@ -7,42 +7,13 @@ const DailyTurnover = require('../daily-turnover/daily-turnover')
 module.exports = {
     dayTurn: async (req, res) => {
 
-        await DailyTurnover.dayTurn()
+        if(req.body.CRON_JOB_GITHUB_KEY === process.env.CRON_JOB_SERVER_KEY){
+            await DailyTurnover.dayTurn()
+            res.json("Successful day change.")
+        } else {
+            res.json("Key is invalid.")
+        }
 
-
-        // console.log("Daily turnover started at " + new Date())
-
-        // //////////// Aging the fish ///////////////
-        // const tanks = await Tank.find()
-
-        // for(let i = 0; i < tanks.length; i++){
-
-        //     let fishInTank = await Fish.find({tankId: tanks[i]._id})
-
-        //     for (let j = 0; j < fishInTank.length; j++){
-        //         await FishClass.ageFish(fishInTank[j], fishInTank, req)
-        //     }
-
-        //     let numberOfFishOwmed = (await Fish.find({ownerId: tanks[i].ownerId})).length
-        //     await User.findOneAndUpdate({_id: tanks[i].ownerId}, {numberOfFish: numberOfFishOwmed})
-        // }
-
-
-        // ////////// Pet shop ///////////////
-        // await Fish.deleteMany({petshopFish: true})
-
-        // let numberOfNewPetshopFish = Math.floor(Math.random() * 3) + 2
-
-        // for (let i = 0; i < numberOfNewPetshopFish; i++){
-        //     const newFish = FishClass.generateRandomFish('petshop')
-        //     Fish.create(newFish)
-        // }
-
-
-        // console.log("Daily turnover complete at " + new Date())
-
-
-        res.json("Successful day change.")
     }
 }
 
--- daily-turnover/daily-turnover-fetch.js
+++ daily-turnover/daily-turnover-fetch.js
@@ -1,5 +1,8 @@
 fetch('https://fishsim.onrender.com/dailyturnover', {
-    method: 'PUT'
+    method: 'PUT',
+    body: JSON.stringify({
+        CRON_JOB_GITHUB_KEY: process.env.CRON_JOB_GITHUB_KEY
+    })
 })
     .then(json => console.log("Successful day change"))
     .catch (err => console.log(err))
