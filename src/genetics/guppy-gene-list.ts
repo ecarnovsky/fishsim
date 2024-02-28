732b8bcf597b2c218f902d70a2c78d0f1fa1c50c
ecarnovsky <ecarnovsky@gmail.com>
2024-02-28 11:27:01 -0500

    Added a few more genes

--- src/genetics/guppy-gene-list.ts
+++ src/genetics/guppy-gene-list.ts
@@ -19,15 +19,15 @@ const GUPPY_GENE_LIST = [
     new Gene("tail angle", GeneTypes.Autosomal, [
         // 100% (base is a full half circle)
         new Allele(undefined, 80, 0.50, 0.90, [
-            new MutatableTrait("tailAnglePercent", TraitTypes.BodyValue, MutationRates.Middle, 100)
+            new MutatableTrait("tailAnglePercent", TraitTypes.BodyValue, MutationRates.Middle, 0)
         ]),
         // 80%
         new Allele(undefined, 50, 0.30, 0.10, [
-            new MutatableTrait("tailAnglePercent", TraitTypes.BodyValue, MutationRates.Middle, 80)
+            new MutatableTrait("varInTailAnglePercent", TraitTypes.BodyValue, MutationRates.Middle, -20)
         ]),
         // 70%
         new Allele(undefined, 30, 0.20, 0.00, [
-            new MutatableTrait("tailAnglePercent", TraitTypes.BodyValue, MutationRates.Middle, 70)
+            new MutatableTrait("varInTailAnglePercent", TraitTypes.BodyValue, MutationRates.Middle, -30)
         ])
     ]),
 
@@ -57,7 +57,40 @@ const GUPPY_GENE_LIST = [
         new Allele('G', 100, 0.30, 0.40, []),
         // doesn't allow blue pigment
         new Allele('g', 0, 0.70, 0.60, [])
-    ])
+    ]),
+
+    new Gene("tuxedo", GeneTypes.XLinked, [
+        // tuxedo / half-black
+        new Allele("Ni2", 70, 0.20, 0.00, [
+            new MutatableTrait("varInTuxTopPoint", TraitTypes.BodyValue, MutationRates.Middle, 0),
+            new MutatableTrait("varInTuxLowPoint", TraitTypes.BodyValue, MutationRates.Middle, 0)
+        ]),
+        // no tuxedo / half-black
+        new Allele("ni2", 0, 0.80, 1, [])
+    ]),
+
+    new Gene("moscow", GeneTypes.YLinked, [
+        // moscow
+        new Allele('Mw', 100, 0.01, 0.00, []),
+        // no moscow
+        new Allele('mw', 0, 0.99, 1, [])
+    ]),    
+    
+    new Gene("albino 1", GeneTypes.Autosomal, [
+        // not albino
+        new Allele('A1', 100, 0.96, 0.99, []),
+        // albino
+        new Allele('a1', 0, 0.04, 0.01, [])
+    ]),
+
+    new Gene("albino 2", GeneTypes.Autosomal, [
+        // not albino
+        new Allele('A2', 100, 0.95, 1, []),
+        // albino
+        new Allele('a2', 0, 0.05, 0.00, [])
+    ]),
+
+
 
 ]
 
