7a54d94073a488df3b32b0a6e6fe95ca44dfaf37
ecarnovsky <ecarnovsky@gmail.com>
2024-04-27 17:53:43 -0400

    Changed the wording on the console log in the daily turnover controller

--- src/controllers/daily-turnover.js
+++ src/controllers/daily-turnover.js
@@ -5,12 +5,12 @@ import * as DailyTurnover from '../daily-turnover/daily-turnover.js'
 module.exports = {
     dayTurn: async (req, res) => {
 
-        if(process.env.CRON_JOB_GITHUB_KEY === process.env.CRON_JOB_SERVER_KEY || req.body.CRON_JOB_GITHUB_KEY && req.body.CRON_JOB_GITHUB_KEY.toString() === process.env.CRON_JOB_SERVER_KEY ){
-            console.log('Key is valid. About to start daily turnover...')
+        if(process.env.CRON_JOB_GITHUB_KEY === process.env.CRON_JOB_SERVER_KEY || req.body.CRON_JOB_GITHUB_KEY == process.env.CRON_JOB_SERVER_KEY ){
+            console.log('Daily turnover key is valid. About to start daily turnover...')
             await DailyTurnover.dayTurn()
             res.json("Daily turnover complete.")
         } else {
-            console.log("Key is invalid.")
+            console.log("Invalid daily turnover key was sent.")
             res.json("Key is invalid.")
         }
     }
