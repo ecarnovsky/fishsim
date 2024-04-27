fd77f47ead40182f215eb3fd26a5b3ef4a1db562
ecarnovsky <ecarnovsky@gmail.com>
2024-04-27 16:06:34 -0400

    Changed the parameter to a string in the daily-turnover controller

--- src/controllers/daily-turnover.js
+++ src/controllers/daily-turnover.js
@@ -5,7 +5,7 @@ import * as DailyTurnover from '../daily-turnover/daily-turnover.js'
 module.exports = {
     dayTurn: async (req, res) => {
 
-        if(req.body.CRON_JOB_GITHUB_KEY === process.env.CRON_JOB_SERVER_KEY || process.env.CRON_JOB_GITHUB_KEY === process.env.CRON_JOB_SERVER_KEY){
+        if(req.body.CRON_JOB_GITHUB_KEY?.toString() === process.env.CRON_JOB_SERVER_KEY || process.env.CRON_JOB_GITHUB_KEY === process.env.CRON_JOB_SERVER_KEY){
             await DailyTurnover.dayTurn()
             res.json("Successful day change.")
         } else {
--- src/daily-turnover/daily-turnover-fetch.js
+++ src/daily-turnover/daily-turnover-fetch.js
@@ -1,8 +1,12 @@
 fetch('https://fishsim.onrender.com/dailyturnover', {
     method: 'PUT',
+    headers: {'Content-type': 'application/json'},
     body: JSON.stringify({
         CRON_JOB_GITHUB_KEY: process.env.CRON_JOB_GITHUB_KEY
     })
 })
-    .then(json => console.log("Successful day change"))
+    .then(json => {
+        console.log("Fetch request done.")
+        console.log(stringify.JSON(json))
+    })
     .catch (err => console.log(err))
