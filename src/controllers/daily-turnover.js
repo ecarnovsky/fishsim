87724ca917c8e627b5042f6eec45e2cf2da3aeca
ecarnovsky <ecarnovsky@gmail.com>
2024-04-27 17:41:45 -0400

    Added console logs to daily-turnover controller

--- src/controllers/daily-turnover.js
+++ src/controllers/daily-turnover.js
@@ -6,12 +6,13 @@ module.exports = {
     dayTurn: async (req, res) => {
 
         if(process.env.CRON_JOB_GITHUB_KEY === process.env.CRON_JOB_SERVER_KEY || req.body.CRON_JOB_GITHUB_KEY && req.body.CRON_JOB_GITHUB_KEY.toString() === process.env.CRON_JOB_SERVER_KEY ){
+            console.log('Key is valid. About to start daily turnover...')
             await DailyTurnover.dayTurn()
-            res.json("Successful day change.")
+            res.json("Daily turnover complete.")
         } else {
+            console.log("Key is invalid.")
             res.json("Key is invalid.")
         }
-
     }
 }
 
